4e4b2e3f06e0b6ff6020f1759f7d4d799819694d
macxk <xiekun132@163.com>
2018-09-02 16:25:58 +0800

    决定做一个vue+springboot 的demo

---
+++ src/main/java/com/kon/sys/web/DataProvideCintroller.java
@@ -0,0 +1,10 @@
+package com.kon.sys.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("dataProvide")
+public class DataProvideCintroller {
+
+}
